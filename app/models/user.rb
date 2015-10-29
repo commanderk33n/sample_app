520b9d384654214a99fd16ccaa37dee367c343c9
eikood <hanfaplast@googlemail.com>
2015-10-29 22:32:54 +0100

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ActiveRecord::Base
-  before_save { self.email = email.downcase}
+  before_save { email.downcase! }
   validates :name, presence: true, length: {maximum: 50}
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: {maximum: 255},
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true,
             format: {with: VALID_EMAIL_REGEX},
-            uniqueness: {case_sensitive: false}
+            uniqueness: {case_sensitive: false}, length: {maximum: 50}
   has_secure_password
   validates :password, presence: true, length: {minimum: 6}
 end
